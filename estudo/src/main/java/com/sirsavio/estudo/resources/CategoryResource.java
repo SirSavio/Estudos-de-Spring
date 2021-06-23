cb68a39fb601547a1f154c108725969d1a03bb49
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-06-23 15:52:27 -0300

    Creating first endpoint

---
+++ estudo/src/main/java/com/sirsavio/estudo/resources/CategoryResource.java
@@ -0,0 +1,14 @@
+package com.sirsavio.estudo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() { 
+		return "Funciona demais";
+	}
+}
