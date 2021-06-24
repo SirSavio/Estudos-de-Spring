6a91ad3a8c9d26f8ebbd7f9f986490f58d8f3246
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-06-24 13:17:22 -0300

    Creating many to many relationship and products

--- estudo/src/main/java/com/sirsavio/estudo/EstudoApplication.java
+++ estudo/src/main/java/com/sirsavio/estudo/EstudoApplication.java
@@ -1,13 +1,47 @@
 package com.sirsavio.estudo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoApplication {
+import com.sirsavio.estudo.domain.Category;
+import com.sirsavio.estudo.domain.Product;
+import com.sirsavio.estudo.repositories.CategoryRepository;
+import com.sirsavio.estudo.repositories.ProductRepository;
 
+@SpringBootApplication
+public class EstudoApplication implements CommandLineRunner {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		Product prod1 = new Product(null, "Computador", 2000.00);
+		Product prod2 = new Product(null, "Impressora", 8000.00);
+		Product prod3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProducts().addAll(Arrays.asList(prod2));
+		
+		prod1.getCategories().addAll(Arrays.asList(cat1));
+		prod2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategories().addAll(Arrays.asList(cat1));
+		
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }
