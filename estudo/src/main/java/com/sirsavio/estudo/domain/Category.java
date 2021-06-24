576c475afa8028c1f2926630ed4002cfbca5c54a
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-06-24 09:35:26 -0300

    Adding db H2 and JPA Category

--- estudo/src/main/java/com/sirsavio/estudo/domain/Category.java
+++ estudo/src/main/java/com/sirsavio/estudo/domain/Category.java
@@ -3,11 +3,17 @@ package com.sirsavio.estudo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
 public class Category implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
