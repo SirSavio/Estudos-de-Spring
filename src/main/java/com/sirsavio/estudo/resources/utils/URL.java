e02f2f5f79428c024cde8b30b98aab42927a0d8e
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-07-05 09:10:29 -0300

    Update URL.java

--- src/main/java/com/sirsavio/estudo/resources/utils/URL.java
+++ src/main/java/com/sirsavio/estudo/resources/utils/URL.java
@@ -4,10 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class URL {
 	public static List<Integer> decodeStringToInteger(String str){
-		return Arrays.asList(str.split(",")).stream().map(s -> Integer.parseInt(s)).toList();
+		return Arrays.asList(str.split(",")).stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
 	}
 	
 	public static String decodeParam(String str) {
