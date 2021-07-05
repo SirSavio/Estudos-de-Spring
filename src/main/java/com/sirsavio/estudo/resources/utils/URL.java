cd854471f3c83c5423be939bf9a2640c8bf3c23a
SirSavio <50343835+SirSavio@users.noreply.github.com>
2021-07-05 11:14:05 -0300

    Create router security

--- src/main/java/com/sirsavio/estudo/resources/utils/URL.java
+++ src/main/java/com/sirsavio/estudo/resources/utils/URL.java
@@ -4,7 +4,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class URL {
